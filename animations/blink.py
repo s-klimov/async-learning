0345a3c6f80092c70261960a24f7e36f81c6ed07
Sergei Kilmov <s.klimov@bk.ru>
2023-02-20 21:54:23 +0700

    lib numpy removed from project

--- animations/blink.py
+++ animations/blink.py
@@ -1,29 +1,37 @@
 import asyncio
 import curses
 
-import numpy as np
-
 from constants import TIC_TIMEOUT
+
+
+def drange(start: float, stop: float, step: float):
+    """
+    Генератор последовательности с дробными элементами
+    """
+    counter = start
+    while counter < stop:
+        yield counter
+        counter += step
 
 
 async def blink(canvas, row, column, symbol="*", delay_periods=1):
     while True:
-        for _ in np.arange(0, 3, TIC_TIMEOUT):
-            canvas.addstr(row, column, symbol, curses.A_DIM)
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        for _ in drange(0, 3, TIC_TIMEOUT):
             await asyncio.sleep(0)
 
+        canvas.addstr(row, column, symbol, curses.A_DIM)
         for _ in range(delay_periods):
-            canvas.addstr(row, column, symbol, curses.A_DIM)
             await asyncio.sleep(0)
 
-        for _ in np.arange(0, 0.3, TIC_TIMEOUT):
-            canvas.addstr(row, column, symbol)
+        canvas.addstr(row, column, symbol)
+        for _ in drange(0, 0.3, TIC_TIMEOUT):
             await asyncio.sleep(0)
 
-        for _ in np.arange(0, 0.5, TIC_TIMEOUT):
-            canvas.addstr(row, column, symbol, curses.A_BOLD)
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        for _ in drange(0, 0.5, TIC_TIMEOUT):
             await asyncio.sleep(0)
 
-        for _ in np.arange(0, 0.3, TIC_TIMEOUT):
-            canvas.addstr(row, column, symbol)
+        canvas.addstr(row, column, symbol)
+        for _ in drange(0, 0.3, TIC_TIMEOUT):
             await asyncio.sleep(0)
