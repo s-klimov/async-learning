37566669256d9d9b121d67d8e85be5a69ebe2c6b
Sergei Kilmov <s.klimov@bk.ru>
2023-02-23 12:30:07 +0700

    step 4 completed

--- animations/ship.py
+++ animations/ship.py
@@ -1,16 +1,18 @@
 import asyncio
-from itertools import repeat
 
-from constants import DIRECTION_STEP, FREQUENCY
+from animations.physics import update_speed
 from curses_tools import draw_frame, read_controls
 
 
 async def animate_spaceship(canvas, border, row, column, ship):
+    row_speed, column_speed = 0, 0
 
     while True:
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
-        row += rows_direction * DIRECTION_STEP
-        column += columns_direction * DIRECTION_STEP
+        row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction, 3, 3)
+
+        row += row_speed
+        column += column_speed
 
         row = min(border.lower - ship.height, max(row, border.upper))
         column = min(border.right - ship.width, max(column, border.left))
